bd587ebeea6616be1968cc7ca5206719e4fd974b
samrocky <56741196+samrocky@users.noreply.github.com>
2019-10-28 00:19:58 +0330

    initial commit

---
+++ Hash.py
@@ -0,0 +1,38 @@
+"""
+How to use :
+copy this file in to your python3's libraries directory
+Linux path :
+Windows path :
+and the you can import " Hash " into your personal prohram
+
+For example :
+input>>> import Hash
+input>>> print(Hash.sha256("sam"))
+output>>> e96e02d8e47f2a7c03be5117b3ed175c52aa30fb22028cf9c96f261563577605
+input>>> print(Hash.md5("sam"))
+output>>> 332532dcfaa1cbf61e2a266bd723612c
+
+Or :
+input>>> from Hash import *
+input>>> print(sha256("sam"))
+output>>> e96e02d8e47f2a7c03be5117b3ed175c52aa30fb22028cf9c96f261563577605
+input>>> print(md5("sam"))
+output>>> 332532dcfaa1cbf61e2a266bd723612c
+
+"""
+
+import hashlib
+
+def sha256(a):
+    data = a
+    m = hashlib.sha256();
+    m.update(data.encode('utf-8'));
+    finaly = m.hexdigest();
+    return finaly
+
+def md5(a):
+    data = a
+    m = hashlib.md5();
+    m.update(data.encode('utf-8'));
+    finaly = m.hexdigest();
+    return finaly
