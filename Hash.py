e0cc70097889b1a9bd204a92ba034cefd9a228d5
samraki <45524208+samraki@users.noreply.github.com>
2019-10-29 00:41:27 +0330

    Solving Issue#1

--- Hash.py
+++ Hash.py
@@ -19,6 +19,15 @@ output>>> e96e02d8e47f2a7c03be5117b3ed175c52aa30fb22028cf9c96f261563577605
 input>>> print(md5("sam"))
 output>>> 332532dcfaa1cbf61e2a266bd723612c
 
+
+        md5
+        sha1
+        sha224
+        sha256
+        sha384
+        sha512
+
+
 """
 
 import hashlib
@@ -33,6 +42,35 @@ def sha256(a):
 def md5(a):
     data = a
     m = hashlib.md5();
+    m.update(data.encode('utf-8'));
+    finaly = m.hexdigest();
+    return finaly
+
+def sha1(a):
+    data = a
+    m = hashlib.sha1();
+    m.update(data.encode('utf-8'));
+    finaly = m.hexdigest();
+    return finaly
+
+def sha224(a):
+    data = a
+    m = hashlib.sha1();
+    m.update(data.encode('utf-8'));
+    finaly = m.hexdigest();
+    return finaly
+
+def sha328(a):
+    data = a
+    m = hashlib.sha1();
+    m.update(data.encode('utf-8'));
+    finaly = m.hexdigest();
+    return finaly
+
+
+def sha512(a):
+    data = a
+    m = hashlib.sha1();
     m.update(data.encode('utf-8'));
     finaly = m.hexdigest();
     return finaly
